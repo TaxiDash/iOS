9be53834885b9204a3a3f5640e1e5bc9fd2461bc
Seth Friedman <sethfri@gmail.com>
2014-02-19 02:49:38 -0600

    Create Driver property in Driver View Controller

--- Taxi Rating/TAXDriverViewController.m
+++ Taxi Rating/TAXDriverViewController.m
@@ -12,6 +12,8 @@
 
 @interface TAXDriverViewController ()
 
+@property (strong, nonatomic) TAXDriver *driver;
+
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UILabel *ratingLabel;
 @property (weak, nonatomic) IBOutlet UILabel *validLabel;
@@ -19,6 +21,18 @@
 @end
 
 @implementation TAXDriverViewController
+
+#pragma mark - Custom Setter
+
+- (void)setDriver:(TAXDriver *)driver {
+    if (![_driver isEqual:driver]) {
+        _driver = driver;
+        
+        self.title = [NSString stringWithFormat:@"%@ %@", driver.firstName, driver.lastName];
+        self.ratingLabel.text = [NSString stringWithFormat:@"%.1f", driver.averageRating];
+        self.validLabel.text = driver.isValid ? @"Valid" : @"Not Valid";
+    }
+}
 
 #pragma mark - View Controller Lifecycle
 
@@ -28,9 +42,7 @@
     [[TAXClient sharedClient] fetchDriverWithID:@"1"
                              andCompletionBlock:^(BOOL success, TAXDriver *driver) {
                                  if (success) {
-                                     self.title = [NSString stringWithFormat:@"%@ %@", driver.firstName, driver.lastName];
-                                     self.ratingLabel.text = [NSString stringWithFormat:@"%.1f", driver.averageRating];
-                                     self.validLabel.text = driver.isValid ? @"Valid" : @"Not Valid";
+                                     self.driver = driver;
                                  } else {
                                      [[[UIAlertView alloc] initWithTitle:@"Failure"
                                                                  message:@"Failed to connect to the server"
